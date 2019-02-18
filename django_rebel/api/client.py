14304512632433eed04cba6675d00bfe947fef10
kalaomer <kalamustafa@gmail.com>
2019-02-18 17:19:27 +0300

    client bug fix

--- django_rebel/api/client.py
+++ django_rebel/api/client.py
@@ -7,11 +7,17 @@ class Client:
     def __init__(self, mailgun):
         self.mailgun = mailgun
 
-    def prepare_request(self, url: str, method="get", data=None, headers=None, params=None):
+    def prepare_request(self, url: str, method="get", data=None, headers=None, params=None, **kwargs):
         real_url = "%s/%s/%s" % (self.API_URL, self.mailgun.profile_settings["API"]["DOMAIN"], url)
 
         auth = ("api", self.mailgun.profile_settings["API"]["API_KEY"])
 
-        req = requests.Request(method=method, url=real_url, auth=auth, data=data, headers=headers, params=params)
+        req = requests.Request(method=method,
+                               url=real_url,
+                               auth=auth,
+                               data=data,
+                               headers=headers,
+                               params=params,
+                               **kwargs)
 
         return req.prepare()
